eeb2fdece2e9d63e53f5f7fdd04899c2791b7da6
Angela Tarque <angela.tarque26@gmail.com>
2025-11-30 16:31:51 -0400

    refactor: Rename CancelarTrabajoContent to CancelarTrabajoPageContent and clean up exports

--- src/app/epic_VisualizadorDeTrabajosAgendadosVistaCliente_CancelarTrabajo/page.tsx
+++ src/app/epic_VisualizadorDeTrabajosAgendadosVistaCliente_CancelarTrabajo/page.tsx
@@ -10,10 +10,7 @@ import ModalCancelar from "./components/ModalCancelar";
 import BotonesAccion from "./components/BotonesAccion";
 import { useSearchParams,useRouter  } from "next/navigation";
 
-
-const CancelarTrabajoContent: React.FC = () => {
 const CancelarTrabajoPageContent: React.FC = () => {
-
   const sp = useSearchParams();
   const router = useRouter(); 
   const [trabajo, setTrabajo] = useState<Trabajo | null>(null);
@@ -138,22 +135,13 @@ useEffect(() => {
   );
 };
 
-
-export default function CancelarTrabajoPage() {
 const CancelarTrabajoPage: React.FC = () => {
-
   return (
     <Suspense fallback={
       <div className="min-h-screen bg-gray-50 flex items-center justify-center">
         <p className="text-gray-600">Cargando...</p>
       </div>
     }>
-
-      <CancelarTrabajoContent />
-    </Suspense>
-  );
-}
-
       <CancelarTrabajoPageContent />
     </Suspense>
   );
