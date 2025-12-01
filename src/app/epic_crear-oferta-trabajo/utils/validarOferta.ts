e583d23addbc7afc0cc5ff78b8ac8022864d4e6b
Jherlan Marcelo Calero Sardan <jherlan.calero@gmail.com>
2025-12-01 19:45:46 -0400

    ajustes en los archivos

--- src/app/epic_crear-oferta-trabajo/utils/validarOferta.ts
+++
@@ -1,8 +0,0 @@
-import { OfertaTrabajo } from "../interfaces/Oferta.interface";
-
-export function validarOferta(data: OfertaTrabajo): string | null {
-  if (!data.titulo.trim()) return "Debe ingresar un título.";
-  if (!data.descripcion.trim()) return "Debe ingresar una descripción.";
-  if (!data.fotoPortada) return "Debe cargar una foto de portada.";
-  return null; // ✔ valido
-}
